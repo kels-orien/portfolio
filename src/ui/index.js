5785dbed8f837357d2a40d21f80666ee259a44fb
kels-orien <kowo4real@yahoo.co.uk>
2020-01-30 22:19:26 +0100

    add  button style

---
+++ src/ui/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export const Button = styled.button`
+    backface-visibility: hidden;
+    background-color: #ec452e;
+    box-sizing: border-box;
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 700;
+    opacity: unset;
+    position: relative;
+    pointer-events: unset;
+    text-align: center;
+    text-transform: uppercase;
+    user-select: none;
+    white-space: unset;
+    color: rgb(255, 255, 255) !important;
+    font-size: 1rem !important;
+    border-radius: 0px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #f83d24;
+    border-image: initial;
+    padding: 1rem 1.5rem;
+    text-decoration: none;
+    transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
+`
