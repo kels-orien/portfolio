753a7e4860ac7182341399f5a0eeb9560f9d8f03
kels-orien <kowo4real@yahoo.co.uk>
2020-02-04 23:57:56 +0100

    add portfolio changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,152 @@
 import React from 'react';
 import './App.css';
-import {Button} from "./ui"
+import arrowRight from "./img/arrow_right.svg"
+import downloadCloud from "./img/download_cloud.svg"
+import Code from "./img/code.svg"
+import { 
+  Header, 
+  Container, 
+  Profile, 
+  ProfileImage, 
+  ProfileImageDiv, 
+  Main, 
+  Dot, 
+  ProfileText, 
+  FontThick, 
+  PText, 
+  Skills, 
+  SkillsTools, 
+  SkillsSection,
+  ToolsSection,
+  STitle,
+  ProjectsSection,
+  ProjectsButton,
+  ProjectButtonImg,
+  ProfileDownloadSection,
+  ProfileButton,
+  ProfileButtonImg,
+  AboutMe,
+  AboutMeText,
+  AboutMeTextHeader,
+  AboutMeTextParagraph,
+  SVGImageSection,
+  SVGImage,
+  WorkSection,
+  WorkHeaderText,
+  WorkExperience,
+  WorkExpSection,
+} from "./ui"
 
 function App() {
   return (
     <div className="App">
-       <Button>I'M AVAILABLE FOR HIRE</Button>
+      <Header>
+        <div className="logo-box">
+          <div className="icon">
+
+            <a className ="icon-name">KORIEN</a>
+            
+          </div>
+          <div className ="menu">
+          <div className = "menu-box">
+              <a className="menu-box-item">Hire Me</a>
+              <a className="menu-box-item">Portfolio</a>
+            </div>
+          </div>
+        </div>
+
+        
+      </Header>
+      <Container>
+      <Main>
+        
+          <Profile>
+            
+              <ProfileImageDiv>
+                <ProfileImage alt="alt=profile-photo" src="https://res.cloudinary.com/dofiasjpi/image/upload/v1580586831/portfolio/headshot.jpg"/>    
+
+              </ProfileImageDiv>
+              <Dot/>
+              <ProfileText><FontThick>Hi 👋,</FontThick><PText>I'm Kohwo Orien a front end engineer currently living in Warri, Nigeria.</PText>
+              <ProfileDownloadSection>
+                <ProfileButton>Résumé
+                <ProfileButtonImg alt="arrow facing right direction" src={downloadCloud}/>
+
+                </ProfileButton>
+
+            </ProfileDownloadSection>
+              </ProfileText>
+            
+          </Profile>
+
+        
+
+      </Main>
+      <AboutMe>
+        <AboutMeText>
+          <AboutMeTextHeader>About Me</AboutMeTextHeader>
+          <AboutMeTextParagraph>I build delightful user interfaces with a professsional and delicate touch. 
+I'm a Javascript nerd that loves solving problems.
+I write acticles about frontend development in medium and other blogs.</AboutMeTextParagraph>
+        </AboutMeText>
+        <SVGImageSection>
+          <SVGImage src={Code} alt="illustration of a software developer"></SVGImage>
+
+      </SVGImageSection>
+      </AboutMe>
+
+      
+     <Skills>
+        <SkillsTools>
+          <SkillsSection>
+            <STitle>Skills</STitle>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>ReactJs</li>
+                <li>NodeJs</li>
+              </ul>
+          </SkillsSection>
+          <ToolsSection>
+          <STitle>Tools</STitle>
+              <ul>
+                <li>Git</li>
+                <li>Heroku</li>
+                <li>Gatsby</li>
+                <li>Netlify</li>
+              </ul>
+          </ToolsSection>
+        </SkillsTools>
+        <ProjectsSection>
+        <ProjectsButton>
+          View Projects
+          <ProjectButtonImg alt="arrow facing right direction" src={arrowRight}/>
+        </ProjectsButton>
+      </ProjectsSection>
+      <WorkSection>
+        <WorkHeaderText>
+          Work Experience
+        </WorkHeaderText>
+        <WorkExperience>
+            <WorkExpSection>
+              <h2>Front-end Developer</h2>
+              <h3>Freelancer</h3>
+              <p>
+                <em>
+                  <time datetime="2017-01">January 2017
+                  </time> -
+                  <time> Present
+                  </time>
+                </em>
+              </p>
+            </WorkExpSection>
+
+        </WorkExperience>
+      </WorkSection>
+     </Skills>
+      
+     </Container>
     </div>
   );
 }
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import {device} from "./device"
 
 export const Button = styled.button`
     backface-visibility: hidden;
@@ -26,3 +26,400 @@ export const Button = styled.button`
     text-decoration: none;
     transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
 `
+export const Header = styled.header`
+    height: 4.4rem;
+    z-index: 1100;
+    background-color: rgb(255, 255, 255);
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    white-space: nowrap;
+    box-shadow: rgba(199, 150, 116, 0.07) 0px 2px 0px 0px;
+    display: block;
+
+    .logo-box {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 48px;
+        padding: 0rem 1rem;
+        -webkit-box-align: center;
+        align-items: center;
+
+        .icon {
+            align-content: flex-start;
+            -webkit-box-align: center;
+            align-items: center;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            -webkit-box-pack: start;
+            justify-content: flex-start;
+            text-transform: capitalize;
+            padding-top: 0rem;
+            padding-right: 0rem;
+            padding-bottom: 0rem;
+            padding-left: 0rem;
+            margin-top: 0rem;
+            margin-right: 0rem;
+            margin-bottom: 0rem;
+            margin-left: 0rem;
+            transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
+
+            .icon-name {
+                display: block;
+                position: relative;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                font-weight: 700 !important;
+                font-size: 1rem !important;
+                line-height: 1.5 !important;
+                margin: 0px;
+                overflow: hidden;
+                transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
+            }
+
+        }
+        .menu {
+            display: flex;
+            flex-direction: row;
+            -webkit-box-align: stretch;
+            align-items: stretch;
+            padding: 0rem 1rem;
+            justify-content: flex-end;
+            -webkit-justify-content: flex-end;
+            -webkit-box-pack: end;
+            display: webkit-flex;
+
+            .menu-box {
+                display: flex;
+                padding: 0.5rem;
+                margin: 0rem 0.5rem;
+    
+                .menu-box-item {
+                    cursor: pointer;
+                    display: inline-block;
+                    max-width: 100%;
+                    text-overflow: ellipsis;
+                    vertical-align: bottom;
+                    font-weight: 700 !important;
+                    font-size: 0.809023rem !important;
+                    line-height: 1.5 !important;
+                    padding: 0.5rem;
+                    margin: 0rem 0.5rem;
+                    overflow: hidden;
+                    text-decoration: none;
+                    transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
+                }
+            } 
+
+        }
+
+        
+    }
+`
+
+export const Main = styled.main`
+    margin-top: 1rem;
+    margin-top: 8rem;
+`
+
+export const Container = styled.div`
+    margin-right: 5vw;
+    margin-left: 5vw;
+
+    ${device.laptop`
+        margin-right: 5vw;
+        margin-left: 5vw;`
+    }
+    ${device.laptopL`
+        width: 1300px;
+        max-width: 1300px;
+        margin: auto;`
+    }
+`
+
+export const Profile = styled.section`
+    display: flex;
+`
+
+
+export const ProfileImage = styled.img`
+    width: 350px;
+    box-shadow: 22px 22px 0 #EFEEEE;
+    ${device.laptop`
+        width: 450px;`
+        }
+`
+
+
+export const Dot =  styled.div`
+    height: 500px;
+    width: 700px;
+    right: 0;
+    position: absolute;
+    z-index: -9;
+    margin-bottom: 15px;
+    background-image: url(https://res.cloudinary.com/dofiasjpi/image/upload/v1580743806/portfolio/plus1.png);
+    background-repeat: repeat;
+
+    ${device.laptop`
+        right: 4vw;`
+    }
+`
+
+
+export const ProfileText = styled.div`
+    height: auto;
+    text-align: right;
+    margin-top: auto;
+    margin-right: 30px;
+    margin-left: auto;
+    margin-bottom: 10px;
+    padding-left: 1.2rem;
+
+
+    ${device.laptop`
+        padding-left: 2rem;
+        margin-bottom: 4rem;`
+    }
+
+    ${device.mobileL`
+    margin-top: 2.3rem;
+    margin-right: 5vw;`
+    }
+`
+
+export const ProfileImageDiv = styled.div`
+    margin-right: 10px;
+`
+
+export const FontThick = styled.h1`
+    font-size: 2rem;
+    color:rgb(76, 76, 76);
+`
+
+export const PText =  styled.p`
+    padding-left: 1.2rem;
+    margin-top: .5rem;
+    font-size: 2rem
+`
+export const ProfileDownloadSection =  styled.div`
+    width: -webkit-fit-content;
+    width: -moz-fit-content;
+    width: fit-content;
+    margin-left: auto;
+    -webkit-transition: -webkit-transform .2s;
+    transition: -webkit-transform .2s;
+    transition: transform .2s;
+    transition: transform .2s,-webkit-transform .2s;
+    -webkit-transition-delay: .01s;
+    transition-delay: .01s;
+`
+export const ProfileButton = styled.div`
+    padding: .7rem 1rem;
+    margin-top: 10px;
+    border-radius: 25px;
+    border: none;
+    background: rgb(5, 45, 73);
+    color: #fff;
+    cursor: pointer;
+    font-size: 1rem;
+    -webkit-box-shadow: 0 9px 20px 0 rgba(52,52,52,.27);
+    box-shadow: 0 9px 20px 0 rgba(52,52,52,.27);
+    text-decoration: none;
+    -webkit-transition: -webkit-box-shadow .2s;
+    transition: -webkit-box-shadow .2s;
+    transition: box-shadow .2s;
+    transition: box-shadow .2s,-webkit-box-shadow .2s;
+    -webkit-transition-delay: .01;
+    transition-delay: .01;
+`
+
+export const ProfileButtonImg =  styled.img`
+    margin-left: 10px;
+    vertical-align: middle;
+  `
+
+export const AboutMe = styled.section`
+    margin-left: 5%;
+    margin-right: 5%;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    margin-top: 7rem;
+`
+export const AboutMeText =  styled.div`
+    display: block
+`
+
+export const AboutMeTextHeader = styled.h1`
+    font-size: 3rem;
+`
+
+export const AboutMeTextParagraph = styled.p`
+    text-align: left;
+    font-size: 1.2rem;
+    font-style: italic;
+    line-height: 1.5rem;
+}
+`
+export const SVGImageSection= styled.section`
+
+    ${device.tablet`
+    margin-top: auto;
+    padding-left: 3rem;
+    ;`
+    }
+    ${device.laptop`
+        margin-top: auto;
+    ;`
+}
+`
+
+export const SVGImage = styled.img`
+    width: 200px;
+
+    ${device.tablet`
+        width: 200px;
+    ;`
+    }
+    ${device.laptop`
+        width: 250px;
+    ;`
+}
+`
+export const STitle = styled.h1`
+        margin-bottom: 1rem;
+        font-size: 1.4rem;
+`
+export const Skills = styled.section`
+    padding-bottom: 1rem;
+    background-image: url(https://res.cloudinary.com/dofiasjpi/image/upload/v1580743806/portfolio/plus1.png);
+    background-repeat: repeat;
+    margin-top: 7rem;
+`
+
+export const SkillsTools= styled.div`
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    padding-top: 3rem;   
+
+    ${device.laptop`
+        -webkit-box-orient: horizontal;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+        flex-direction: row;
+        -ms-flex-pack: distribute;
+        justify-content: space-around;`
+    }
+    `
+
+export const SkillsSection = styled.div`
+    background: #eeeeee;
+    width: 250px;
+    height: 250px;
+    border-radius: 3rem;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    padding-left: 3.5rem;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    line-height: 1.2rem;
+    box-shadow: 0 20px 50px hsla(0,0%,69.4%,.7);
+`
+
+export const ToolsSection = styled.div`
+    background: rgb(5, 45, 73);
+    color:  white;
+    width: 250px;
+    height: 250px;
+    border-radius: 3rem;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    padding-left: 3.5rem;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    line-height: 1.2rem;
+    box-shadow: 0 20px 50px hsla(0,0%,69.4%,.7);
+`
+
+export const ProjectsSection = styled.section`
+    margin-top: 3rem;
+    width: -webkit-fit-content;
+    width: -moz-fit-content;
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+
+export const ProjectsButton =  styled.a`
+    background: rgb(5, 45, 73);
+    color:  white;
+    display: inline-block;
+    padding: .6rem .75rem;
+    text-decoration: none;
+    -webkit-box-shadow: 0 20px 50px hsla(0,0%,69.4%,.7);
+    box-shadow: 0 20px 50px hsla(0,0%,69.4%,.7);
+    border-radius: 3rem;
+    -webkit-transition: -webkit-transform .2s;
+    transition: -webkit-transform .2s;
+    transition: transform .2s;
+    transition: transform .2s,-webkit-transform .2s;
+    -webkit-transition-delay: .01s;
+    transition-delay: .01s;
+`
+  export const ProjectButtonImg =  styled.img`
+     vertical-align: middle;
+     padding-left: .2rem;
+  `
+
+  export const WorkSection =  styled.section`
+     padding-bottom: 1rem;
+     margin-top: 7rem;
+
+     
+    ${device.tablet`
+        padding-right: 5%;
+        padding-left: 5%;`
+    }
+  `
+
+  export const WorkHeaderText = styled.h1`
+    font-size: 3rem;
+  `
+  export const WorkExperience = styled.div`
+    display: block;
+  `
+  export const WorkExpSection = styled.section`
+    background: #fff;
+    padding: 2rem 1em;
+    margin-bottom: 3rem;
+    border-radius: 2rem;
+    -webkit-box-shadow: 0 20px 50px hsla(0,0%,87.8%,.7);
+    box-shadow: 0 20px 50px hsla(0,0%,87.8%,.7);
+    border: none;
+    line-height:2rem;
+    margin-top: 3rem;
+  `
